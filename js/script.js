98e1a8c8741b7fee40bdb14fb4f828afe7ceec01
yasnovello <yasnovello@users.noreply.github.com>
2018-03-15 17:20:06 -0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$('#changeicon1').click(function(){
+  $('#icon1').toggleClass('fa fa-arrow-down fa fa-arrow-up');    
+});
+
+$('#changeicon2').click(function(){
+  $('#icon2').toggleClass('fa fa-arrow-down fa fa-arrow-up');    
+});
+
+$('#changeicon3').click(function(){
+  $('#icon3').toggleClass('fa fa-arrow-down fa fa-arrow-up');    
+});
